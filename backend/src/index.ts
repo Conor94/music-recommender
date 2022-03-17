e95afb0ce6e7f3e258e307232f96adb6f6b253fb
Conor Barr <cbarr1881@conestogac.on.ca>
2022-03-17 19:33:58 -0400

    Routes for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,15 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import config from "./config";
+import router from "./routes/router";
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use("/api", router);
 
 app.listen(config.port, config.host, () => {
     console.log(`Server is up on ${config.host}:${config.port}`);
